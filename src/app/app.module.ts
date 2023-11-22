cee5f28153be82aebe1e30e42570b80b6d023e87
Р34-Антон <r34-anton@monowork.pro>
2023-11-22 15:05:27 +0300

    added prefix template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import {Module} from '@nestjs/common';
 import { TelegramModule } from '@telegram/telegram.module';
 import { ConfigModule } from '@nestjs/config';
 import {UserModule} from "@src/api/user/user.module";
+import {RouterModule} from "@nestjs/core";
 
 @Module({
-  imports: [ConfigModule.forRoot(), TelegramModule, UserModule]
+  imports: [
+    ConfigModule.forRoot(),
+    TelegramModule,
+    UserModule,
+    RouterModule.register([
+      {
+        path: 'dashboard',
+        module: UserModule,
+      },
+    ]),
+  ],
 })
 export class AppModule {}
